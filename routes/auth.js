7aa2413e7948c1f0549dd8176f38a94af713d7e6
saurabharch <saurabhkashyap0001@gmail.com>
2018-03-18 14:03:08 +0530

    google auth updated

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 
 module.exports = router;
