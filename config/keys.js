6150c6e864bfa18f9562a62fe640373ce4d31c3f
saurabharch <saurabhkashyap0001@gmail.com>
2018-03-19 00:59:27 +0530

    started UI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://saurabh:Kash007@ds217349.mlab.com:17349/storybooks-dev',
+  mongoURI: 'mongodb://saurabh:kash007@ds217349.mlab.com:17349/storybooks-dev',
   googleClientID:'802924828587-2bpnbrp10s8bjn2d04aej4h4685gmk9i.apps.googleusercontent.com',
   googleClientSecret:'-A_Gvq1CxHRvza1cxVw7XNVi'
 }
