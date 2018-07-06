7226d0f49a021e9c7b2a7e32c5800d5809a6dac5
saurabharch <saurabhkashyap0001@gmail.com>
2018-07-07 01:14:53 +0530

    update production google production key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI || 'mongodb://saurabh:kash007@ds217349.mlab.com:17349/storybooks-prod',
-    googleClientID: process.env.GOOGLE_CLIENT_ID || '940186458258-s4m0j36osepijik5k19bkaifa91dr997.apps.googleusercontent.com',
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || 'xYwQ7Dd5CRWE2Le91-VMMvSl',
+    googleClientID: process.env.GOOGLE_CLIENT_ID || '477995149649-i60ar391u3hcagi6913mvmkcr15053m6.apps.googleusercontent.com',
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || 'roLhwsJ9w6wynRBhPi4TC2rK',
     clientID: '178884466091426',  
     clientSecret: '0a7ba0991aab6a90081f0c0792974c5e'
 }
