2094c4b4756c72e5522a84724e803e98e6262667
saurabharch <saurabhkashyap0001@gmail.com>
2018-07-07 01:28:29 +0530

    production keys is updated

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI || 'mongodb://saurabh:kash007@ds217349.mlab.com:17349/storybooks-prod',
-    googleClientID: process.env.GOOGLE_CLIENT_ID || '477995149649-i60ar391u3hcagi6913mvmkcr15053m6.apps.googleusercontent.com',
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || 'roLhwsJ9w6wynRBhPi4TC2rK',
+    mongoURI: 'mongodb://saurabh:kash007@ds021046.mlab.com:21046/storybooks-prod-new', //process.env.MONGO_URI ||
+    googleClientID: '477995149649-i60ar391u3hcagi6913mvmkcr15053m6.apps.googleusercontent.com', //process.env.GOOGLE_CLIENT_ID ||
+    googleClientSecret: 'roLhwsJ9w6wynRBhPi4TC2rK', //process.env.GOOGLE_CLIENT_SECRET ||
     clientID: '178884466091426',  
     clientSecret: '0a7ba0991aab6a90081f0c0792974c5e'
 }
