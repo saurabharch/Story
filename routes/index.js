2e0486910729859fbdc67104535b574019d5c1c9
saurabharch <saurabhkashyap0001@gmail.com>
2018-08-27 13:13:48 +0530

    seo is fixed achive at maximum lavel

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ const router = express.Router();
 const { ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 router.get('/', ensureGuest, (req, res) => {
+    res.locals.metaTags = {
+        title: 'StoryBook',
+        description: 'StoryBook is an award winning blog that talks about living a boss free life with blogging. We cover about WordPress, SEO, Make money Blogging, Affiliate marketing.',
+        keywords: 'Affiliate Marketing,Money Making, Online Earning, Blog, Science and Technology,Software and web application development',
+        generator: 'Story Book MetaTag Generator v.1.0',
+        author: 'Saurabh Kashyap'
+    };
     res.render('index/welcome');
 });
 
