451e85bda0681dc64062a063ac4b6e28a75e4de9
saurabharch <saurabhkashyap0001@gmail.com>
2018-09-09 00:10:21 +0530

    update workbox

--- src/sw.js
+++ src/sw.js
@@ -72,49 +72,6 @@ self.addEventListener('notificationclick', function (event) {
         })
     );
 });
-
-// if ('serviceWorker' in navigator && 'PushManager' in window) {
-//     console.log('Service Worker and Push is supported');
-
-//     navigator.serviceWorker.register('/hello.js')
-//         .then(function (swReg) {
-//             console.log('service worker registered');
-
-//             swRegistration = swReg;
-
-//             swRegistration.pushManager.getSubscription()
-//                 .then(function (subscription) {
-//                     isSubscribed = !(subscription === null);
-
-//                     if (isSubscribed) {
-//                         console.log('User is subscribed');
-//                     } else {
-//                         swRegistration.pushManager.subscribe({
-//                                 userVisibleOnly: true,
-//                                 applicationServerKey: urlB64ToUint8Array(applicationKey)
-//                             })
-//                             .then(function (subscription) {
-//                                 console.log(subscription);
-//                                 console.log('User is subscribed');
-
-//                                 saveSubscription(subscription);
-
-//                                 isSubscribed = true;
-//                             })
-//                             .catch(function (err) {
-//                                 console.log('Failed to subscribe user: ', err);
-//                             })
-//                     }
-//                 })
-//         })
-//         .catch(function (error) {
-//             console.error('Service Worker Error', error);
-//         });
-
-
-// } else {
-//     console.warn('Push messaging is not supported');
-
 function saveSubscription(subscription) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.open("POST", "/subscribe");
